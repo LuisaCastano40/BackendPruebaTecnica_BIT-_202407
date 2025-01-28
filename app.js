4dab397b826ca3409f5159794d8736e8ab2ca451
Luisa Castano <loise.0607@gmail.com>
2025-01-27 20:03:23 -0500

    cambios para funcionamiento back

--- app.js
+++ app.js
@@ -17,6 +17,9 @@ app.use(cors());
 app.use(express.json());
 app.use('/usuarios', usersRouter);
 app.use('/iniciarSesion', loginRouter);
+app.get('/', (req, res)=>{
+    res.send('Backed funcionando correctamente');
+});
 
 
 // Exportar la aplicación sin iniciar el servidor
